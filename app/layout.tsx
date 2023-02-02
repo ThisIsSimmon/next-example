733039c18a048c9be4851325a110f9b3fe13a46d
Simmon <thisissimmon@gmail.com>
2023-02-02 16:15:44 +0900

    :white_check_mark: test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
+import "server-only";
 import "./globals.css";
+import { Noto_Sans_JP } from "@next/font/google";
+
+export const revalidate = 0;
+
+const notoSansJP = Noto_Sans_JP({
+    weight: ["500", "700"],
+    preload: false,
+    display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="ja" className={notoSansJP.className}>
             {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
