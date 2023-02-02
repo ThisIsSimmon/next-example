bdb5f6369c001c1246f9b574e6e4b38ed56560d8
Simmon <thisissimmon@gmail.com>
2023-02-02 16:24:01 +0900

    :white_check_mark: test

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ const notoSansJP = Noto_Sans_JP({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="ja" className={notoSansJP.className}>
+        <html lang="ja">
+            {/* <html lang="ja" className={notoSansJP.className}> */}
             {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
